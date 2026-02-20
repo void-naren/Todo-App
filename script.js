2f09acd52b50b3d5ac22e8abc44ec975b130985d
void-naren <singhnarender2810@gmail.com>
2026-02-20 06:06:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if (task === "") return;
+
+    let li = document.createElement("li");
+    li.textContent = task;
+
+    let btn = document.createElement("button");
+    btn.textContent = "X";
+    btn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(btn);
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
